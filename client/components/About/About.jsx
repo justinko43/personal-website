c410369b7a0ca1ba0bc0bf7d31a8f49bfc5f36a8
justin.ko43 <justin.ko43@gmail.com>
2018-02-12 13:24:28 -0800

    added typing functionality to about page

--- client/components/About/About.jsx
+++ client/components/About/About.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import Typist from 'react-typist';
+import TypistLoop from 'react-typist-loop';
 
 const About = () => (
   <div>
-    Justin Ko
+    <h2>Justin Ko</h2>
+    {/* <TypistLoop>
+      {[
+        'Entrepreneur',
+        'Full-Stack Engineer',
+        '(Future) Shiba Owner',
+      ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
+    </TypistLoop> */}
   </div>
 )
 
