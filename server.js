c3ecadc67531ccd29ed4a996e869e57aa882411d
justin.ko43 <justin.ko43@gmail.com>
2018-02-12 16:48:42 -0800

    changed port error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 //server setup
-const SERVER_PORT = process.env.port || 3000;
+const SERVER_PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use('/build', express.static(path.resolve(__dirname, './build')));
