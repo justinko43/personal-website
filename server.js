ebd3a0c316ceedb8215ce79ff1c70ca73cf38e34
justin.ko43 <justin.ko43@gmail.com>
2018-02-12 16:45:12 -0800

    server change

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
 
 //server setup
 const SERVER_PORT = process.env.port || 3000;
 const app = express();
 
-// app.use(favicon(path.resolve(__dirname, '/build/assets/images', 'favicon.ico')));
 app.use('/build', express.static(path.resolve(__dirname, './build')));
 
 app.get('*', (req, res) => {
