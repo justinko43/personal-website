f01a2bc1df91190f863a8dba366450ed02babc7a
justin.ko43 <justin.ko43@gmail.com>
2018-02-13 17:10:49 -0800

    made some logo changes

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo2.svg';
 
 const Navbar = (props) => (
-  <div id="navbar" className="bg-white padding-left-xl padding-right-xl">
-    <div id="logo-container">
-      <a href="#" onClick={props.onLogoClick}><img style={{ width: '75px'}} src={logo} /></a>
+  <div id="navbar" className="bg-white margin-top-lg padding-left-xl padding-right-xl">
+    <div id="logo-container" className="center-content">
+      <a href="#" onClick={props.onLogoClick}><img style={{ width: '200px'}} src={logo} /></a>
     </div>
-    <nav id="navigation" className="margin-right-xl">
-      <ul className="fw-600">
-        <span>
-          <li className="hide-nav-link animate"> <a href="#" onClick={props.onWorkClick}>Work </a></li>
-          <li className="hide-nav-link animate"> <a href="#" onClick={props.onWritingClick}>Writing</a></li>
-          <li className="hide-nav-link animate"> <a href="#" onClick={props.onContactClick}>Contact</a></li>
+    <nav id="navigation" className="margin-top-m center-content">
+      <ul className="fw-600 center-content">
+        <span className="center-content">
+          <li className="hide-nav-link animate "> <a href="#" onClick={props.onWorkClick}>Work </a></li>
+          <li className="hide-nav-link animate "> <a href="#" onClick={props.onWritingClick}>Writing</a></li>
+          <li className="hide-nav-link animate "> <a href="#" onClick={props.onContactClick}>Contact</a></li>
         </span>
       </ul>
     </nav>
