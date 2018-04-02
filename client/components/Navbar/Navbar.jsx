6787723d2a8ecc07e74fdb4be4e804c73a052219
justin.ko43 <justin.ko43@gmail.com>
2018-04-02 14:51:49 -0700

    refactor

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -3,19 +3,34 @@ import { render } from 'react-dom';
 
 import logo from '../../assets/logo2.svg';
 
-const Navbar = (props) => (
-  <div id="navbar" className="bg-white margin-top padding-left-xl padding-right-xl">
-    <div id="logo-container" className="center-content">
-      <a href="#" onClick={props.onLogoClick}><img style={{ width: '200px'}} src={logo} /></a>
-    </div>
-    <ul id="navigation" className="margin-top center-content">
-      <span>
-        <li className="hide-nav-link animate"> <a href="#" onClick={props.onWorkClick}>Work </a></li>
-        <li className="hide-nav-link animate"> <a href="#" onClick={props.onWritingClick}>Writing</a></li>
-        <li className="hide-nav-link animate"> <a href="#" onClick={props.onContactClick}>Contact</a></li>
-      </span>
-    </ul>
-  </div>
-)
+const Navbar = props => (
+	<div id="navbar" className="bg-white margin-top padding-left-xl padding-right-xl">
+		<div id="logo-container" className="center-content">
+			<a href="#" onClick={props.onLogoClick}>
+				<img style={{ width: '200px' }} src={logo} />
+			</a>
+		</div>
+		<ul id="navigation" className="margin-top center-content">
+			<span>
+				<li className="hide-nav-link animate">
+					<a href="#" onClick={props.onWorkClick}>
+						Work
+					</a>
+				</li>
+				<li className="hide-nav-link animate">
+					
+					<a href="#" onClick={props.onWritingClick}>
+						Writing
+					</a>
+				</li>
+				<li className="hide-nav-link animate">
+					<a href="#" onClick={props.onContactClick}>
+						Contact
+					</a>
+				</li>
+			</span>
+		</ul>
+	</div>
+);
 
 export default Navbar;
