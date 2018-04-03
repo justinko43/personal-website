f67c016ad081f376a04ac36fa55e184e957520ec
justin.ko43 <justin.ko43@gmail.com>
2018-04-03 12:50:58 -0700

    Add overlay working

--- client/components/Work/Project.jsx
+++ client/components/Work/Project.jsx
@@ -3,8 +3,13 @@ import { render } from 'react-dom';
 import speakgenius from '../../assets/sg_icon_512.png'
 const Project = (props) => (
   <div className="project" id={`project-${props.number}`}>
-    <img style={{width: 100}} src={speakgenius}/>
-    {/* {props.directory} */}
+    <div className="front">
+      <img style={{width: 100}} src={speakgenius}/>
+      <p>{props.description}</p>
+    </div>
+    <div className="overlay">
+      <p>back</p>
+    </div>
   </div>
 )
 
