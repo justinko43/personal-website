9b6f959aa7cfc1c6539e73078fe8a52e082417e4
justin.ko43 <justin.ko43@gmail.com>
2018-04-03 12:58:16 -0700

    adjust image width on project

--- client/components/Work/Project.jsx
+++ client/components/Work/Project.jsx
@@ -4,7 +4,7 @@ import speakgenius from '../../assets/sg_icon_512.png'
 const Project = (props) => (
   <div className="project" id={`project-${props.number}`}>
     <div className="front">
-      <img style={{width: 100}} src={speakgenius}/>
+      <img style={{width: 125}} src={speakgenius}/>
       <p>{props.description}</p>
     </div>
     <div className="overlay">
