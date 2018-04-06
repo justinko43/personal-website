da8bcf21a248e064b00bc5c9932787cc3068ba79
justin.ko43 <justin.ko43@gmail.com>
2018-04-06 13:37:25 -0700

    Add title description

--- client/components/Work/Project.jsx
+++ client/components/Work/Project.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import speakgenius from '../../assets/sg_icon_512.png'
+
+
 const Project = (props) => (
   <div className="project" id={`project-${props.number}`}>
     <div className="front">
-      <img style={{width: 125}} src={speakgenius}/>
-      <p>{props.description}</p>
+      <img src={speakgenius}/>
+      <div className="work-description">
+        <h4 className="fw-600">{props.title}</h4>
+        <p>{props.description}</p>
+      </div>
     </div>
     <div className="overlay">
       <p>back</p>
