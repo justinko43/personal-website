ce7fb38e4719869db9745b65f2a1486177a53210
justin.ko43 <justin.ko43@gmail.com>
2018-04-06 13:37:12 -0700

    refactor navbar

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 
 import logo from '../../assets/logo2.svg';
 
+
 const Navbar = props => (
 	<div id="navbar" className="bg-white margin-top padding-left-xl padding-right-xl">
 		<div id="logo-container" className="center-content">
